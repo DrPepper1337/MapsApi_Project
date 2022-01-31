e4fdcc9803c38c75065e0ce62b8ea9b229d2fa9d
KonstantaBit <jaykonstanta@mail.ru>
2022-01-31 17:17:24 +0300

    task_2

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 import os
 import sys
 
-import pygame
+import pygame as pg
 import requests
 
-map_request = "https://static-maps.yandex.ru/1.x/?ll=133.795393,-25.694776&z=3&l=sat"
-response = requests.get(map_request)
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+pg.init()
+screen = pg.display.set_mode((600, 450))
+running = True
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+lon = "133.795393"
+lat = "-25.694776"
+delta = 0.00
 
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
-os.remove(map_file)
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_PAGEUP:
+                delta += 0.02
+            if event.key == pg.K_PAGEDOWN and delta != 0:
+                delta -= 0.02
+    api_server = "http://static-maps.yandex.ru/1.x/"
+    params = {
+        "ll": ",".join([lon, lat]),
+        "spn": ",".join([str(delta), str(delta)]),
+        "l": "sat"
+    }
+    response = requests.get(api_server, params=params)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    screen.blit(pg.image.load(map_file), (0, 0))
+    os.remove(map_file)
+    pg.display.flip()
+
+pg.quit()
